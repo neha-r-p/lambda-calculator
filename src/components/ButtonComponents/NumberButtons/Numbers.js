6cb473bee1022d22040fa438350691bde1d66802
Neha Patel <neha.r.p@gmail.com>
2019-07-16 13:53:35 -0700

    WIP imported data to relevant js files (Numbers, Operators, and Specials

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -4,6 +4,8 @@ import React from "react";
 
 //Import your array data to from the provided data file
 
+import { numbers } from '../../../data'
+
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   return (
