877096ba63be8d5c39d10b08a363bad7ca7ceee9
Neha Patel <neha.r.p@gmail.com>
2019-07-16 17:12:16 -0700

    WIP style: display styled

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Display = () => {
 
   const [displayState, setDisplayState] = useState(0);
-  return <div>{displayState}</div>;
+  return <div className="display">{displayState}</div>;
 };
 
 export default Display;
